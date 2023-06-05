ebf17af737f382b40ec055374e8a88aee86af547
tszwong <wongt@bu.edu>
2023-06-05 01:08:27 -0400

    adding spring project 02

---
+++ 02-spring-boot-spring-core/pokemondemo/src/test/java/com/springbootlearning/pokemondemo/PokemondemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootlearning.pokemondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemondemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
