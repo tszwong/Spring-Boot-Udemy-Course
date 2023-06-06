1f46fba287730a856cbb8f4a663358f55e44bc6c
tszwong <wongt@bu.edu>
2023-06-06 17:57:21 -0400

    finished constructor injection section of course

--- 02-spring-boot-spring-core/01-constructor-injection/pokemondemo/src/main/java/com/springbootlearning/pokemondemo/WaterPokemon.java
+++ 02-spring-boot-spring-core/01-constructor-injection/pokemondemo/src/main/java/com/springbootlearning/pokemondemo/WaterPokemon.java
@@ -1,4 +1,11 @@
 package com.springbootlearning.pokemondemo;
 
-public class WaterPokemon {
+import org.springframework.stereotype.Component;
+
+@Component // marks the class as a Spring bean, makes it available for dependency injection
+public class WaterPokemon implements Pokemon{
+    @Override
+    public String getDailyPokemon() {
+        return "You get a Squirtle";
+    }
 }
