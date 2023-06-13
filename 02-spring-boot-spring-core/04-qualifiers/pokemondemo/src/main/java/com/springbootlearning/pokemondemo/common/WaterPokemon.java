5b2490fca7c80de31aeafab2a559a5140aee9c0b
tszwong <wongt@bu.edu>
2023-06-13 16:18:21 -0400

    Qualifier and Primary section done

--- 02-spring-boot-spring-core/04-qualifiers/pokemondemo/src/main/java/com/springbootlearning/pokemondemo/common/WaterPokemon.java
+++ 02-spring-boot-spring-core/04-qualifiers/pokemondemo/src/main/java/com/springbootlearning/pokemondemo/common/WaterPokemon.java
@@ -1,8 +1,10 @@
 package com.springbootlearning.pokemondemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component // marks the class as a Spring bean, makes it available for dependency injection
+@Primary
 public class WaterPokemon implements Pokemon {
     @Override
     public String getDailyPokemon() {
--- 02-spring-boot-spring-core/04-qualifiers/pokemondemo/src/main/java/com/springbootlearning/pokemondemo/rest/PokemonController.java
+++ 02-spring-boot-spring-core/04-qualifiers/pokemondemo/src/main/java/com/springbootlearning/pokemondemo/rest/PokemonController.java
@@ -2,7 +2,7 @@ package com.springbootlearning.pokemondemo.rest;
 
 import com.springbootlearning.pokemondemo.common.Pokemon;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +11,14 @@ public class PokemonController {
     // create a private field for dependency
     private Pokemon myPokemon;
 
-    @Autowired // constructor injection
-    public void PokemonController(@Qualifier("waterPokemon") Pokemon thePokemon) {
+//    @Autowired // constructor injection
+//    public void PokemonController(@Qualifier("waterPokemon") Pokemon thePokemon) {
+//        myPokemon = thePokemon;
+//    }
+
+    // an alternative to @Qualifier we can mark a bean as the @Primary
+    @Autowired
+    public void PokemonController(Pokemon thePokemon) {
         myPokemon = thePokemon;
     }
 
