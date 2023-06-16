e333eb3804043ed0495a4355168cd778fb8dfad8
tszwong <wongt@bu.edu>
2023-06-16 18:25:39 -0400

    Starting Hibernate section

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-pokemon/src/main/java/com/springbootlearning/pokedexcruddemo/PokedexcruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.springbootlearning.pokedexcruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexcruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexcruddemoApplication.class, args);
+	}
+
+}
